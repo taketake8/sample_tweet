3c84d141d07e49ca0e81779af1d43e0d5e9b9991
taketake8 <62596306+taketake8@users.noreply.github.com>
2020-06-13 14:35:00 +0900

    モデル作成
    user,tweet,commnt

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Comment extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
